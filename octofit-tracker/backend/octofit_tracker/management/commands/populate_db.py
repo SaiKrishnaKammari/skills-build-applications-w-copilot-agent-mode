fabaa78d278449bd39c4723d9f5830018995ad94
SaiKrishnaKammari <101502604+SaiKrishnaKammari@users.noreply.github.com>
2025-11-03 10:21:30 +0000

    new changes

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -6,9 +6,48 @@ class Command(BaseCommand):
     help = 'Populate the octofit_db database with test data'
 
     def handle(self, *args, **options):
-        # Clear existing data in correct order
+        # Clear existing data
         Leaderboard.objects.all().delete()
         Activity.objects.all().delete()
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        users = [
+            User(name='Spider-Man', email='spiderman@marvel.com', team=marvel),
+            User(name='Iron Man', email='ironman@marvel.com', team=marvel),
+            User(name='Wonder Woman', email='wonderwoman@dc.com', team=dc),
+            User(name='Batman', email='batman@dc.com', team=dc),
+        ]
+        for user in users:
+            user.save()
+
+        # Create workouts
+        workouts = [
+            Workout(name='Pushups', description='Do 20 pushups', suggested_for='Strength'),
+            Workout(name='Running', description='Run 2 miles', suggested_for='Cardio'),
+        ]
+        for workout in workouts:
+            workout.save()
+
+        # Create activities
+        Activity.objects.create(user=users[0], type='Running', duration=30, date=timezone.now().date())
+        Activity.objects.create(user=users[1], type='Pushups', duration=15, date=timezone.now().date())
+        Activity.objects.create(user=users[2], type='Running', duration=25, date=timezone.now().date())
+        Activity.objects.create(user=users[3], type='Pushups', duration=20, date=timezone.now().date())
+
+        # Create leaderboard
+        Leaderboard.objects.create(user=users[0], score=100)
+        Leaderboard.objects.create(user=users[1], score=80)
+        Leaderboard.objects.create(user=users[2], score=90)
+        Leaderboard.objects.create(user=users[3], score=85)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
         for user in User.objects.all():
             user.delete()
         Workout.objects.all().delete()
